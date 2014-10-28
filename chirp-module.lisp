9fa833b3b87cd840a50b2520d6be5243c611b1f4
Phil Newton <phil@sodaware.net>
2014-10-27 21:45:24 -0400

    Moved template rendering out of main app

--- chirp-module.lisp
+++ chirp-module.lisp
@@ -1,6 +1,7 @@
 (restas:define-module
  #:chirp
- (:use :cl :restas))
+ (:use :cl :restas)
+ (:export #:chirp-render-template))
 
 (in-package #:chirp)
 
@@ -9,5 +10,11 @@
 
 (defparameter *asset-directory*
   (merge-pathnames #P"assets/" chirp-config:*base-directory*))
+
+(defun chirp-render-template (name params)
+  (with-open-file (template-file name)
+                  (let ((template (make-string (file-length template-file))))
+                    (read-sequence template template-file)
+                    (funcall (cl-template:compile-template template) params))))
 
 (setq *show-lisp-errors-p* t)
--- chirp.lisp
+++ chirp.lisp
@@ -11,11 +11,8 @@
 ;; Set up some routes
 (restas:define-route
  homepage ("")
- (with-open-file (template-file "templates/index.html.clt")
-                 (let ((template (make-string (file-length template-file)))
-                       (text "Hello, World!"))
-                   (read-sequence template template-file)
-                   (funcall (cl-template:compile-template template) (list :text text)))))
+ (chirp-render-template "templates/index.html.clt"
+                        (list :text "Hello World")))
 
 ;; Start chirp on 8080 (so nginx can proxy it)
 ;;(start '#:chirp-app :port 8080)
